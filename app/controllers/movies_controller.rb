cb86ad244dc8b14ffb5a7cf0b8694e11c5eb3d23
Alex <alexvdberg010109@gmail.com>
2024-09-18 11:34:24 +0200

    added images and more db tables

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -35,5 +35,6 @@ private
 
 def movie_params
     params.require(:movie).
-    permit(:title, :description, :rating, :released_on, :total_gross)
+      permit(:title, :description, :rating, :released_on, :total_gross,
+              :director, :duration, :image_file_name)
 end
