d9df7743ff44176d71212975209deb5985ec0444
Alex <alexvdberg010109@gmail.com>
2024-09-18 12:12:18 +0200

    added flashes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,21 +13,27 @@ class MoviesController < ApplicationController
     end
     def update
         @movie = Movie.find(params[:id])
-        @movie.update(movie_params)
-        redirect_to @movie
+        if @movie.update(movie_params)
+          redirect_to @movie, notice: "Movie successfully updated!"
+        else
+          render :edit, status: :unprocessable_entity
+        end
     end
 
     def create
         @movie = Movie.new(movie_params)
-        @movie.save
-        redirect_to @movie
+        if @movie.save
+          redirect_to @movie, notice: "Movie successfully created!"
+        else
+          render :new, status: :unprocessable_entity
+        end
     end
 
     def destroy
         @movie = Movie.find(params[:id])
         @movie.destroy
-
-        redirect_to movies_url, status: :see_other
+        redirect_to movies_url, status: :see_other,
+          alert: "Movie successfully deleted!"
     end
 end
 
